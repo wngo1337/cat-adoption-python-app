a1d01d4020652cefbb9f25729f83b8ca6d4ed00d
wngo1337 <wngo1337@gmail.com>
2024-06-11 15:45:09 -0400

    Add continous two-way conversation feature with cats   -Tweak cat wisdom prompt and add context-awareness to replies by   passing chat history to Gemini call   -Add server-side sessions using Flask-Session with Redis for temp   storage of user-cat interactions required for above (Flask session cache size limited to 4 KB too small)

--- app/authentication.py
+++ app/authentication.py
@@ -5,52 +5,59 @@ from . import db
 from .models import Cat, User
 from werkzeug.security import generate_password_hash, check_password_hash
 
-authentication = Blueprint('authentication', __name__)
+authentication = Blueprint("authentication", __name__)
 
-@authentication.route('/login')
+
+@authentication.route("/login")
 def login():
-    return render_template('login.html')
+    return render_template("login.html")
 
-@authentication.route('/login', methods=['POST'])
+
+@authentication.route("/login", methods=["POST"])
 def login_post():
-    username = request.form.get('username')
-    raw_password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
+    username = request.form.get("username")
+    raw_password = request.form.get("password")
+    remember = True if request.form.get("remember") else False
 
     user = User.query.filter_by(username=username).first()
 
     if not user or not check_password_hash(user.password, raw_password):
         flash("You have entered invalid login details. Please try again.")
-        return redirect(url_for('authentication.login'))
+        return redirect(url_for("authentication.login"))
 
     login_user(user, remember=remember)
-    return redirect(url_for('main.profile'))
+    return redirect(url_for("main.profile"))
 
-@authentication.route('/signup')
+
+@authentication.route("/signup")
 def signup():
-    return render_template('signup.html')
+    return render_template("signup.html")
 
-@authentication.route('/signup', methods=['POST'])
+
+@authentication.route("/signup", methods=["POST"])
 def signup_post():
     # Make sure user with that username does not already exist
-    username = request.form.get('username')
-    password = request.form.get('password')
+    username = request.form.get("username")
+    password = request.form.get("password")
 
     user = User.query.filter_by(username=username).first()
     if user:
         flash("A user with that username already exists!")
-        return redirect(url_for('authentication.signup'))
+        return redirect(url_for("authentication.signup"))
 
     new_user = User(
         username=username,
-        password=generate_password_hash(password, method='pbkdf2:sha256'))
+        password=generate_password_hash(password, method="pbkdf2:sha256"),
+    )
 
     db.session.add(new_user)
     db.session.commit()
-    return redirect((url_for('authentication.login')))
+    return redirect((url_for("authentication.login")))
 
-@authentication.route('/logout')
+
+@authentication.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect(url_for("main.index"))
+
